e160e23fc6951cd9c8db2cea8bea81c2a4144f5c
Justin Branch <justinms@outlook.com>
2020-01-25 05:39:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,10 +10,15 @@ class Triangle
   def kind
     if @sides.uniq.length == 1
       return :equilateral
+    elsif @sides.uniq.length == 2 
+      return :isosceles
     elsif @sides.uniq.length == 3 
       return :scalene
-    elsif 
+    end 
+  end
+  
+  class TriangleError < StandardError
+    
+  end 
 end
 
-tri = Triangle.new(10, 10, 10)
-p tri.all
