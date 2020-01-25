500f7636b18e854553eecf8437eb9618001f5391
Justin Branch <justinms@outlook.com>
2020-01-25 05:15:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,15 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
-  
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end 
   
-  def kind
-    
+  def self.all
+    @@all
+  end 
 end
+
+
