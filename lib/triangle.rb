3701f001aa787d970c3ad46e4b18e92781d5df29
Justin Branch <justinms@outlook.com>
2020-01-25 05:23:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,15 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  
+  @sides = []
 
   def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    @sides << side1, side2, side3
   end 
-
   
+  def all 
+    @sides
+  end 
 end
 
-
+tri = Triangle.new(10,10,10)
+tri.all
