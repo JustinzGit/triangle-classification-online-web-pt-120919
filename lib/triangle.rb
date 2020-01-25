8e1dfda7c7d3ac52ba1ac33971c9b3086d104439
Justin Branch <justinms@outlook.com>
2020-01-25 05:23:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,9 @@ class Triangle
   @sides = []
 
   def initialize(side1, side2, side3)
-    @sides << side1, side2, side3
+    @sides << side1
+    @sides << side2
+    @sides << side3
   end 
   
   def all 
